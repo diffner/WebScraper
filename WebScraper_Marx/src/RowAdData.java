a5b5166670a9fcc2148190de6212e30e29493b31
hoffek <hoffek@kth.se>
2019-05-07 15:42:50 +0200

    Class RowAdData created.

---
+++ WebScraper_Marx/src/RowAdData.java
@@ -0,0 +1,23 @@
+/**
+ * A class that will create object for storing the row data of each advertisement as text.
+ */
+public class RowAdData {
+    private String title;
+    private String discription;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+}
